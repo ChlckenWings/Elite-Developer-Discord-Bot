ac0c19026b12d9b6917a6edaa44436dba45956a5
Lyxnx <a.julnes@gmail.com>
2016-10-15 19:57:40 +0100

    Edit startup methods

--- src/main/java/com/collectivedev/bot/Main.java
+++ src/main/java/com/collectivedev/bot/Main.java
@@ -1,41 +1,63 @@
 package com.collectivedev.bot;
 
+import com.collectivedev.bot.command.CommandDispatcher;
+import com.collectivedev.bot.command.classes.EchoCommand;
 import com.collectivedev.bot.json.JsonConfiguration;
-import com.collectivedev.bot.logging.BotLogger;
 import com.collectivedev.bot.persist.Database;
+import com.collectivedev.bot.task.TaskManager;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import net.dv8tion.jda.JDA;
+import net.dv8tion.jda.JDABuilder;
 
+import javax.security.auth.login.LoginException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Main.getInstance().start();
     }
 
     private static Main instance;
 
-    private final JsonConfiguration config;
-    private final Database database;
-    private final BotLogger logger;
+    private JDA jda;
+
+    private JsonConfiguration config;
+    private Database database;
     private final BotManager botManager;
+    private final TaskManager taskManager;
 
     private Main() {
         instance = this;
 
-        this.logger = new BotLogger("Bot", "bot.log");
+        this.botManager = new BotManager();
+        this.taskManager = new TaskManager();
+    }
 
+    private void start() throws LoginException, InterruptedException {
         this.config = new JsonConfiguration("config.json");
+
+        this.config.copyDefaults("defaultconfig.json");
+
+        this.jda = new JDABuilder().setBotToken(config.getString("token")).buildBlocking();
+
+        this.jda.addEventListener(new CommandDispatcher());
 
         this.database = new Database(this.config.get("database").getAsJsonObject());
 
-        this.botManager = new BotManager();
-    }
-
-    private void start() {
-        this.config.copyDefaults("defaultconfig.json");
         this.database.createTables();
+        this.registerCommands();
+        this.botManager.init();
+
+        this.jda.getGuilds().forEach(guild ->
+                botManager.getCommands().keySet().forEach(key ->
+                        initDefaultCommands(guild.getId(), key)
+                )
+        );
     }
 
     public static Main getInstance() {
@@ -50,16 +72,16 @@ public class Main {
         return database;
     }
 
-    public BotLogger getLogger() {
-        return logger;
-    }
-
     public BotManager getBotManager() {
         return botManager;
     }
 
     public JsonConfiguration getJsonConfiguration() {
         return config;
+    }
+
+    public TaskManager getTaskManager() {
+        return taskManager;
     }
 
     private ExecutorService service;
@@ -72,5 +94,33 @@ public class Main {
         }
 
         return service;
+    }
+
+    public JDA getJda() {
+        return jda;
+    }
+
+    private void registerCommands() {
+        botManager.registerCommand(new EchoCommand());
+    }
+
+    private void initDefaultCommands(String server, String command) {
+        PreparedStatement ps = null;
+
+        try (Connection conn = Main.getInstance().getDatabase().getConnection()) {
+            ps = conn.prepareStatement("INSERT IGNORE INTO `commands`(`server_id`, `command_name`, `power`) VALUES(?, ?, ?);");
+
+            ps.setString(1, server);
+            ps.setString(2, command.toLowerCase());
+            ps.setInt(3, 100);
+
+            ps.executeUpdate();
+
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            Database.close(ps);
+        }
     }
 }
