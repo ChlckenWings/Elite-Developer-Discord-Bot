971f58972c4d993cbcf3bcde271691fb6d034c5a
Lyxnx <a.julnes@gmail.com>
2016-10-15 15:21:03 +0100

    Edit database class

--- src/main/java/com/collectivedev/bot/persist/Database.java
+++ src/main/java/com/collectivedev/bot/persist/Database.java
@@ -1,7 +1,5 @@
 package com.collectivedev.bot.persist;
 
-import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.Table;
 import com.google.gson.JsonObject;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
@@ -10,13 +8,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Map;
 import java.util.stream.Stream;
 
-public class Database implements AbstractDatabase {
+public class Database {
 
     private final HikariDataSource source;
 
@@ -70,79 +65,5 @@ public class Database implements AbstractDatabase {
         } catch (SQLException | IOException e) {
             e.printStackTrace();
         }
-    }
-
-    @Override
-    public Table<String, String, Character> getAllChannelIDs() {
-        Table<String, String, Character> table = HashBasedTable.create();
-
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try(Connection conn = getConnection()) {
-            ps = conn.prepareStatement("SELECT * FROM `channels`;");
-
-            rs = ps.executeQuery();
-
-            while(rs.next()) {
-                table.put(rs.getString("server_id"), rs.getString("channel_id"), rs.getString("channel_type").toCharArray()[0]);
-            }
-
-            rs.close();
-            ps.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            close(rs, ps);
-        }
-
-        return table;
-    }
-
-    @Override
-    public Table<String, String, Character> getAllRoleIDs() {
-        Table<String, String, Character> table = HashBasedTable.create();
-
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try(Connection conn = getConnection()) {
-            ps = conn.prepareStatement("SELECT * FROM `roles`;");
-
-            rs = ps.executeQuery();
-
-            while(rs.next()) {
-                table.put(rs.getString("server_id"), rs.getString("role_id"), rs.getString("role_type").toCharArray()[0]);
-            }
-
-            rs.close();
-            ps.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            close(rs, ps);
-        }
-
-        return table;
-    }
-
-    @Override
-    public Map<String, Character> getChannelsForServer(String server) {
-        return getAllChannelIDs().row(server);
-    }
-
-    @Override
-    public Map<String, Character> getRolesForServer(String server) {
-        return getAllRoleIDs().row(server);
-    }
-
-    @Override
-    public char getChannelType(String server, String channel) {
-        return getChannelsForServer(server).get(channel);
-    }
-
-    @Override
-    public char getRoleType(String server, String role) {
-        return getRolesForServer(server).get(role);
     }
 }
