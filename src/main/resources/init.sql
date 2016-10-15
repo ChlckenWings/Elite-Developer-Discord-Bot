387c9d9380d35fc646458eda04c558bd3ffdd38e
Lyxnx <a.julnes@gmail.com>
2016-10-15 14:59:22 +0100

    Add init.sql to create tables on startup

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,17 +1,17 @@
-CREATE TABLE IF NOT EXISTS `channels`(
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `server_id` VARCHAR(255) NOT NULL,
-  `channel_id` VARCHAR(255) NOT NULL,
-  `channel_type` CHAR(1) NOT NULL,
-  PRIMARY KEY(`id`),
-  UNIQUE(`server_id`, `channel_id`, `channel_type`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `roles`(
+CREATE TABLE IF NOT EXISTS `role_power`(
   `id` INT NOT NULL AUTO_INCREMENT,
   `server_id` VARCHAR(255) NOT NULL,
   `role_id` VARCHAR(255) NOT NULL,
-  `role_type` CHAR(1) NOT NULL,
+  `power` INT NOT NULL,
   PRIMARY KEY(`id`),
-  UNIQUE(`server_id`, `role_id`, `role_type`)
+  UNIQUE(`server_id`, `role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `commands`(
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `server_id` VARCHAR(255) NOT NULL,
+  `command_name` VARCHAR(255) NOT NULL,
+  `power` INT NOT NULL,
+  PRIMARY KEY(`id`),
+  UNIQUE(`server_id`, `command_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
